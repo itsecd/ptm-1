24fb96b0e4bfada7bfd312ea071171770596c6de
Santas7 <andreypanyshkin03@gmail.com>
2023-09-11 21:37:29 +0400

    1 - исправил в соответствии с PEP отступы между строками кода.

--- main.py
+++ main.py
@@ -3,31 +3,21 @@ import time
 import random
 import datetime
 import requests
-
-
 import json
 import csv
 import sqlite3
-
-
 import pandas as pd
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
 
 
-
-
 class КНИГА:
     def __init__(self, title, author, publication_year, isbn, genre, pages):
         self.title = title  # Название
-
         self.author = author  # Автор
-
         self.publication_year = publication_year  # Год издания
-
         self.isbn = isbn  # ISBN
         self.genre = genre  # Жанр
-
         self.pages = pages  # Количество страниц
 
     def __str__(self):
@@ -53,7 +43,6 @@ class БИБЛИОТЕКА_2_0:
     def add_book(self, book):
         self.books.append(book)
 
-    # Удаляем книгу из библиотеки
     def find_books_by_author(self, author):
         '''
         Возвращает список книг, написанных указанным автором
@@ -62,7 +51,6 @@ class БИБЛИОТЕКА_2_0:
         '''
         return [book for book in self.books if book.author == author]
 
-    # Удаляем книгу из библиотеки
     def find_books_published_after(self, year):
         '''
         Возвращает список книг, выпущенных после указанного года
@@ -99,7 +87,6 @@ class БИБЛИОТЕКА_2_0:
     def get_genres(self):
         return list(set([book.genre for book in self.books]))
 
-# Дополнительные функции и классы
 
 class УНИВЕРСАЛЬНОЕ_ЧТЕНИЕ_ФАЙЛА:
     def __init__(self, filename):
@@ -135,7 +122,6 @@ class УНИВЕРСАЛЬНОЕ_ЧТЕНИЕ_ФАЙЛА:
         with sqlite3.connect(self.filename) as connection:
             return pd.read_sql_query("SELECT * FROM table", connection)
 
-
     def read_xml(self):
         '''
         Чтение XML файла
@@ -146,7 +132,6 @@ class УНИВЕРСАЛЬНОЕ_ЧТЕНИЕ_ФАЙЛА:
         root = tree.getroot()
         return root
 
-
     def read_yaml(self):
         '''
         Чтение YAML файла
@@ -156,30 +141,23 @@ class УНИВЕРСАЛЬНОЕ_ЧТЕНИЕ_ФАЙЛА:
         with open(self.filename, 'r') as file:
             return yaml.load(file, Loader=yaml.FullLoader)
 
-
     def read_pickle(self):
         import pickle
         with open(self.filename, 'rb') as file:
             return pickle.load(file)
 
-
-
     def read_word(self):
         import docx
         return docx.Document(self.filename)
-
 
     def read_image(self):
         from PIL import Image
         return Image.open(self.filename)
 
 
-
 def CREAT3E_sample_():
     # Создаем библиотеку
     БИБЛИОТЕКА = БИБЛИОТЕКА_2_0()
-
-    # Создаем книги
     book1 = КНИГА(
         "THE BOOK 1",
         "John. T",
@@ -187,7 +165,6 @@ def CREAT3E_sample_():
         "8274827611",
         "Fiction",
         220)
-
     book2 = КНИГА(
         "THE BOOK 2",
         "L.L. Horsev",
@@ -195,9 +172,6 @@ def CREAT3E_sample_():
         "9876543210",
         "Fiction",
         280)
-
-
-
     book3 = КНИГА(
         "NOU NAME",
         "G.G. Irooskf",
@@ -205,9 +179,6 @@ def CREAT3E_sample_():
         "5432109876",
         "Dystopian",
         320)
-
-
-
     book4 = КНИГА(
         "OKAY BOOK",
         "T.T. Orlov",
@@ -215,7 +186,6 @@ def CREAT3E_sample_():
         "5442449876",
         "Detective",
         250)
-
     book5 = КНИГА(
         "TAYLER",
         "-",
@@ -223,7 +193,6 @@ def CREAT3E_sample_():
         "8787319875",
         "Detective",
         500)
-
     book6 = КНИГА(
         "TAYLER2",
         "-",
@@ -231,34 +200,25 @@ def CREAT3E_sample_():
         "8799319875",
         "Detective",
         650)
-
     БИБЛИОТЕКА.add_book(book1)
     БИБЛИОТЕКА.add_book(book2)
-
     БИБЛИОТЕКА.add_book(book3)
-
     БИБЛИОТЕКА.add_book(book4)
     БИБЛИОТЕКА.add_book(book5)
     БИБЛИОТЕКА.add_book(book6)
-
     return БИБЛИОТЕКА
 
 def main():
     # Создаем приложение PyQt
     app = QApplication(sys.argv)
-
     window = QWidget()
     window.setWindowTitle('Библиотека 2.0')
-
     # Создаем библиотеку и добавляем книги
     БИБЛИОТЕКА = CREAT3E_sample_()
-
     # Создаем макет для отображения книг
     layout = QVBoxLayout()
-
     # Выводим книги по автору
     author_label = QLabel("Books by John. T:")
-
     layout.addWidget(author_label)
 
     for book in БИБЛИОТЕКА.find_books_by_author("John. T"):
@@ -266,44 +226,33 @@ def main():
             str(book))
         # Добавляем книгу на макет
         layout.addWidget(book_label)
-
     # Выводим книги, выпущенные после 1950 года
     year_label = (
         QLabel("Books published after 1950:"))
-
     layout.addWidget(year_label)
-
     for book in БИБЛИОТЕКА.find_books_published_after(1950):
         book_label = QLabel(str(book))
         # Добавляем книгу на макет
         layout.addWidget(book_label)
-
     # Выводим общее количество страниц
     total_pages_label = QLabel(f"Total Pages in Library: {БИБЛИОТЕКА.get_total_pages()}")
-
     layout.addWidget(total_pages_label)
-
     # Выводим жанры
     genres_label = QLabel(f"Genres in Library: {', '.join(БИБЛИОТЕКА.get_genres())}")
     layout.addWidget(genres_label)
-
     # Добавляем макет на окно
     for i in range(10):
         dummy_label = QLabel(f"Dummy Label {i}")
         layout.addWidget(dummy_label)
-
     # Добавляем макет на окно
     window.setLayout(layout)
     # Отображаем окно
     window.show()
-
     # Запускаем приложение
     sys.exit(app.exec())
 
 if __name__ == "__main__":
     # Запускаем программу
     time.sleep(1)
-
     main()
-
     time.sleep(1)
