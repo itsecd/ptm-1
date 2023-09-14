ec8c4705e75412a33772e724a2f8eb1a6398ec76
pavel <tomashaytis@gmail.com>
2023-09-14 14:04:33 +0400

    Начал работу с импортами. Убрал шаблон "import *" и изменил в соответствии с этим программу. Перенёс всё, что не относится к функциям в main.

--- L-system.py
+++ L-system.py
@@ -1,13 +1,5 @@
-from turtle import *
+import turtle
 from random import randint
-hideturtle()
-color('black')
-tracer(0)
-seth(90)
-penup()
-setpos(0, -250)
-pendown()
-width(1)
 
 
 def init_pythagoras_tree():
@@ -215,164 +207,171 @@ def tree_rules(prev_genome):
 	return new_genome
 
 
-def run_pythagoras_tree(genome, length, wid):
+def run_pythagoras_tree(genome, length, width):
 	stack = []
 	for i in range(len(genome)):
-		width(wid)
+		turtle.width(width)
 		if genome[i] == '0':
-			color('green')
-			forward(length+20)
-			color('black')
+			turtle.color('green')
+			turtle.forward(length+20)
+			turtle.color('black')
 		if genome[i] == '1':
-			forward(length)
+			turtle.forward(length)
 		if genome[i] == '[':
-			wid -= 2
-			stack.append(str(xcor()) + ' ' + str(ycor()) + ' ' + str(heading()) + ' ' + str(wid))
-			seth(heading() + 30)
+			width -= 2
+			stack.append(str(turtle.xcor()) + ' ' + str(turtle.ycor()) + ' ' + str(turtle.heading()) + ' ' + str(width))
+			turtle.seth(turtle.heading() + 30)
 		if genome[i] == ']':
 			t = list(map(float, stack[-1].split()))
 			stack.pop(-1)
-			wid = t[3]
-			penup()
-			setpos(t[0], t[1])
-			pendown()
-			seth(t[2]-30)
+			width = t[3]
+			turtle.penup()
+			turtle.setpos(t[0], t[1])
+			turtle.pendown()
+			turtle.seth(t[2]-30)
 
 
 def run_branch(genome, length, angle):
 	stack = []
 	for i in range(len(genome)):
 		if genome[i] == 'F':
-			forward(length)
+			turtle.forward(length)
 		elif genome[i] == '[':
-			stack.append(str(xcor()) + ' ' + str(ycor()) + ' ' + str(heading()))
-			seth(heading())
+			stack.append(str(turtle.xcor()) + ' ' + str(turtle.ycor()) + ' ' + str(turtle.heading()))
+			turtle.seth(turtle.heading())
 		elif genome[i] == ']':
 			t = list(map(float, stack[-1].split()))
 			stack.pop(-1)
-			penup()
-			setpos(t[0], t[1])
-			pendown()
-			seth(t[2])
+			turtle.penup()
+			turtle.setpos(t[0], t[1])
+			turtle.pendown()
+			turtle.seth(t[2])
 		elif genome[i] == '-':
-			left(angle)
+			turtle.left(angle)
 		elif genome[i] == '+':
-			right(angle)
+			turtle.right(angle)
 	
 
 def run_tree(genome, length, angle, width):
 	stack = []
 	for i in range(len(genome)):
-		width(width)
+		turtle.width(width)
 		if genome[i] == '0':
-			q = pensize()
-			width(4)
+			q = turtle.pensize()
+			turtle.width(4)
 			r = randint(0, 10)
 			if r < 3:
-				pencolor('#009900')
+				turtle.pencolor('#009900')
 			elif r > 6:
-				pencolor('#667900')
+				turtle.pencolor('#667900')
 			else:
-				pencolor('#20BB00')
-			forward(length-2)
-			width(q)
-			color('black')
+				turtle.pencolor('#20BB00')
+			turtle.forward(length-2)
+			turtle.width(q)
+			turtle.color('black')
 		elif genome[i] == '^':
 			tmp_angle = randint(-30, 30)
 			if tmp_angle < 0:
-				left(tmp_angle - 25)
+				turtle.left(tmp_angle - 25)
 			else:
-				left(tmp_angle + 25)
+				turtle.left(tmp_angle + 25)
 		elif genome[i] == '1':
 			if randint(0, 10) > 4:
-				forward(length)
+				turtle.forward(length)
 		elif genome[i] == '2':
 			if randint(0, 10) > 4:
-				forward(length)
+				turtle.forward(length)
 		elif genome[i] == '[':
 			width *= 0.75
-			stack.append(str(xcor()) + ' ' + str(ycor()) + ' ' + str(heading()) + ' ' + str(width))
+			stack.append(str(turtle.xcor()) + ' ' + str(turtle.ycor()) + ' ' + str(turtle.heading()) + ' ' + str(width))
 		elif genome[i] == ']':
 			t = list(map(float, stack[-1].split()))
 			stack.pop(-1)
-			penup()
-			setpos(t[0], t[1])
-			pendown()
-			seth(t[2])
+			turtle.penup()
+			turtle.setpos(t[0], t[1])
+			turtle.pendown()
+			turtle.seth(t[2])
 			width = t[3]
 		elif genome[i] == '+':
-			right(angle+randint(-13, 13))
+			turtle.right(angle+randint(-13, 13))
 		elif genome[i] == '-':
-			left(angle+randint(-13, 13))
+			turtle.left(angle+randint(-13, 13))
 		else:
 			if randint(0, 10) > 4:
-				forward(length)
+				turtle.forward(length)
 
 
 def run_apple(genome, length, angle, width):
 	stack = []
 	for i in range(len(genome)):
-		width(width)
+		turtle.width(width)
 		if genome[i] == '0':
-			q = pensize()
-			width(4)
+			q = turtle.pensize()
+			turtle.width(4)
 			r = randint(0, 10)
 			if r < 3:
-				pencolor('#009900')
+				turtle.pencolor('#009900')
 			elif r > 6:
-				pencolor('#667900')
+				turtle.pencolor('#667900')
 			else:
-				pencolor('#20BB00')
+				turtle.pencolor('#20BB00')
 			if 7 > len(stack) > 2 and randint(0, 100) < 7:
-				w = heading()
-				seth(-90)
-				width(2)
-				color('black')
-				forward(5)
-				pencolor('#992200')
-				width(7)
-				forward(length-9)
-				seth(w)
+				w = turtle.heading()
+				turtle.seth(-90)
+				turtle.width(2)
+				turtle.color('black')
+				turtle.forward(5)
+				turtle.pencolor('#992200')
+				turtle.width(7)
+				turtle.forward(length-9)
+				turtle.seth(w)
 			else:
-				forward(length-2)
-			width(q)
-			color('black')
+				turtle.forward(length-2)
+			turtle.width(q)
+			turtle.color('black')
 		elif genome[i] == '^':
 			tmp_angle = randint(-30, 30)
 			if tmp_angle < 0:
-				left(tmp_angle - 25)
+				turtle.left(tmp_angle - 25)
 			else:
-				left(tmp_angle + 25)
+				turtle.left(tmp_angle + 25)
 		elif genome[i] == '1':
 			if randint(0, 10) > 4:
-				forward(length)
+				turtle.forward(length)
 		elif genome[i] == '2':
 			if randint(0, 10) > 4:
-				forward(length)
+				turtle.forward(length)
 		elif genome[i] == '[':
 			width *= 0.75
-			stack.append(str(xcor()) + ' ' + str(ycor()) + ' ' + str(heading()) + ' ' + str(width))
+			stack.append(str(turtle.xcor()) + ' ' + str(turtle.ycor()) + ' ' + str(turtle.heading()) + ' ' + str(width))
 		elif genome[i] == ']':
 			t = list(map(float, stack[-1].split()))
 			stack.pop(-1)
-			penup()
-			setpos(t[0], t[1])
-			pendown()
-			seth(t[2])
+			turtle.penup()
+			turtle.setpos(t[0], t[1])
+			turtle.pendown()
+			turtle.seth(t[2])
 			width = t[3]
 		elif genome[i] == '+':
-			right(angle+randint(-13, 13))
+			turtle.right(angle+randint(-13, 13))
 		elif genome[i] == '-':
-			left(angle+randint(-13, 13))
+			turtle.left(angle+randint(-13, 13))
 		else:
 			if randint(0, 10) > 4:
-				forward(length)
+				turtle.forward(length)
 
 
 if __name__ == "__main__":
+	turtle.hideturtle()
+	turtle.color('black')
+	turtle.tracer(0)
+	turtle.seth(90)
+	turtle.penup()
+	turtle.setpos(0, -250)
+	turtle.pendown()
+	turtle.width(1)
 	tree_genome, tree_length, tree_angle, tree_iter_number, tree_width = init_tree()
 	for j in range(tree_iter_number):
 		tree_genome = tree_rules(tree_genome)
 	run_apple(tree_genome, tree_length, tree_angle, tree_width)
-	update()
-	mainloop()
+	turtle.done()
