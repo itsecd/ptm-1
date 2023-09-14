c775d6dc6b437deb50e609e084d1d1c9730fc48f
razzzenya <buckingkiska@gmail.com>
2023-09-14 17:49:55 +0400

    Привёл все кавычки в файлах к единому виду.

--- lab_2/class_iterator.py
+++ lab_2/class_iterator.py
@@ -8,12 +8,12 @@ class DateIterator:
     def __init__(self):
 
         self.counter = 0
-        self.file_name = 'result.csv'
+        self.file_name = "result.csv"
 
     def __next__(self) -> tuple:
 
         if os.path.exists(self.file_name):
-            with open(self.file_name, 'r', encoding='utf-8') as csvfile:
+            with open(self.file_name, "r", encoding="utf-8") as csvfile:
                 reader_object = list(csv.reader(csvfile, delimiter=","))
 
                 if self.counter == len(reader_object):
@@ -33,14 +33,14 @@ class DateIteratorFromXY:
     def __init__(self):
 
         self.counter = 0
-        self.X = 'divide_data_output//X.csv'
-        self.Y = 'divide_data_output//Y.csv'
+        self.X = "divide_data_output//X.csv"
+        self.Y = "divide_data_output//Y.csv"
 
     def __next__(self) -> tuple:
 
         if os.path.exists(self.X) and os.path.exists(self.Y):
 
-            with open(self.X, 'r', encoding='utf-8') as csvfile:
+            with open(self.X, "r", encoding="utf-8") as csvfile:
                 reader_object = list(csv.reader(csvfile, delimiter=","))
 
             if self.counter == len(reader_object):
@@ -48,13 +48,13 @@ class DateIteratorFromXY:
 
             elif self.counter < len(reader_object):
                 self.counter += 1
-                with open(self.X, 'r', encoding='utf-8') as csvfilex:
+                with open(self.X, "r", encoding="utf-8") as csvfilex:
 
                     reader_object_X = list(
                         csv.reader(csvfilex, delimiter=","))
                     date = reader_object_X[self.counter][0]
 
-                with open(self.Y, 'r', encoding='utf-8') as csvfiley:
+                with open(self.Y, "r", encoding="utf-8") as csvfiley:
 
                     reader_object_Y = list(
                         csv.reader(csvfiley, delimiter=","))
@@ -68,7 +68,7 @@ class DateIteratorFromXY:
 class DateIteratorFromWeeks:
     def __init__(self):
 
-        self.file_name = 'data_to_weeks_output'
+        self.file_name = "data_to_weeks_output"
         self.counter = 0
         self.data = []
 
@@ -77,7 +77,7 @@ class DateIteratorFromWeeks:
             for root, dirs, files in os.walk(self.file_name):
                 for file in files:
 
-                    with open(os.path.join(self.file_name, file), 'r', encoding='utf-8') as csvfile:
+                    with open(os.path.join(self.file_name, file), "r", encoding="utf-8") as csvfile:
                         dates = list(csv.reader(csvfile, delimiter=","))
 
                         for i in range(len(dates)):
@@ -100,7 +100,7 @@ class DateIteratorFromWeeks:
 class DateIteratorFromYears:
     def __init__(self):
 
-        self.file_name = 'data_to_years_output'
+        self.file_name = "data_to_years_output"
         self.counter = 0
         self.data = []
 
@@ -109,7 +109,7 @@ class DateIteratorFromYears:
             for root, dirs, files in os.walk(self.file_name):
                 for file in files:
 
-                    with open(os.path.join(self.file_name, file), 'r', encoding='utf-8') as csvfile:
+                    with open(os.path.join(self.file_name, file), "r", encoding="utf-8") as csvfile:
                         dates = list(csv.reader(csvfile, delimiter=","))
 
                         for i in range(len(dates)):
--- lab_2/data_to_weeks.py
+++ lab_2/data_to_weeks.py
@@ -15,10 +15,10 @@ def get_year_from_data(data: list[list[str]], index: int) -> int:
         int: Value of year
     """
     date = data[index][0]
-    year = ''
+    year = ""
     for numbers in date:
 
-        if numbers != '-':
+        if numbers != "-":
             year += numbers
 
         else:
@@ -37,11 +37,11 @@ def get_month_from_data(data: list[list[str]], index: int) -> int:
         int: Value of month
     """
     date = data[index][0]
-    month = ''
+    month = ""
     dash_counter = 0
     for numbers in date:
 
-        if numbers == '-':
+        if numbers == "-":
             dash_counter += 1
             continue
 
@@ -65,10 +65,10 @@ def get_day_from_data(data: list[list[str]], index: int) -> int:
         int: Value of day
     """
     date = data[index][0]
-    day = ''
+    day = ""
     dash_counter = 0
     for numbers in date:
-        if numbers == '-':
+        if numbers == "-":
             dash_counter += 1
             continue
 
@@ -81,14 +81,14 @@ def name_for_file(first_part: str, second_part: str) -> str:
     """Creates a name for file
 
     Args:
-        first_part (str): First part of future file's name
-        second_part (str): Second part of future file's name
+        first_part (str): First part of future file"s name
+        second_part (str): Second part of future file"s name
     Returns:
         str: Name in special format
     """
-    f_p = first_part.replace('-', '') + '_'
-    s_p = second_part.replace('-', '') + '.csv'
-    return os.path.join('data_to_weeks_output', f_p + s_p)
+    f_p = first_part.replace("-", "") + "_"
+    s_p = second_part.replace("-", "") + ".csv"
+    return os.path.join("data_to_weeks_output", f_p + s_p)
 
 
 def week_border(data: list[list[str]], index) -> list[str]:
@@ -143,8 +143,8 @@ def weeks_writer(data: list[list[str]]) -> NoReturn:
         data (list[list[str]]): A list with dates and data
     """
 
-    first_part_of_name = ''
-    second_part_of_name = ''
+    first_part_of_name = ""
+    second_part_of_name = ""
     output = []
     border = week_border(data, 0)
     is_first = True
@@ -168,8 +168,8 @@ def weeks_writer(data: list[list[str]]) -> NoReturn:
         if day_in_week == False:
 
             border = week_border(data, i)
-            with open(name_for_file(first_part_of_name, second_part_of_name), 'w', encoding='utf-8') as csv_file:
-                writer = csv.writer(csv_file, lineterminator='\n')
+            with open(name_for_file(first_part_of_name, second_part_of_name), "w", encoding="utf-8") as csv_file:
+                writer = csv.writer(csv_file, lineterminator="\n")
                 for j in output:
                     writer.writerow((j))
                 output = []
@@ -189,20 +189,20 @@ def data_to_weeks(file_name: str):
 
     if os.path.exists(file_name):
 
-        if not os.path.exists('data_to_weeks_output'):
-            os.mkdir('data_to_weeks_output')
+        if not os.path.exists("data_to_weeks_output"):
+            os.mkdir("data_to_weeks_output")
 
-        with open(file_name, 'r', encoding='utf-8') as csvfile:
+        with open(file_name, "r", encoding="utf-8") as csvfile:
             reader_object = list(csv.reader(csvfile, delimiter=","))
             weeks_writer(reader_object)
 
     else:
         raise FileNotFoundError
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        file_name = 'result.csv'
+        file_name = "result.csv"
         data_to_weeks(file_name)
 
     except FileNotFoundError:
-        print('No such file exists!')
+        print("No such file exists!")
