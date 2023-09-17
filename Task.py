72b5fac7d41154abcb5e76fd3a880216932fffff
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2023-09-18 00:03:35 +0400

    fix: comments and docstrings

--- Task.py
+++ Task.py
@@ -3,33 +3,79 @@
 # Blockchain parser
 # Copyright (c) 2015-2023 Denis Leonov <466611@gmail.com>
 #
+# Данный код имеет несколько нарушений стиля PEP 8:
+
+# Пробелы вокруг операторов: В некоторых местах не хватает пробелов вокруг операторов. Например, перед и после операторов = и :.
+
+# Имена переменных: Имена переменных и функций должны быть в snake_case (снейк-регистре), то есть разделяться символами подчеркивания _, а не в camelCase (смешанный регистр).
+
+# Пропущенные пустые строки: В некоторых местах кода не хватает пустых строк для лучшей читаемости.
+
+# Избыточные точки с запятой: В Python точки с запятой в конце строк обычно не используются, за исключением случаев, когда необходимо разместить несколько выражений в одной строке.
+
+# Длинные строки: Некоторые строки кода слишком длинные, что затрудняет их чтение. Рекомендуется разбивать длинные строки на более короткие.
+
+# Неиспользуемые переменные: В коде есть переменные, которые объявляются, но не используются (например, переменная nameRes).
+
+# \/ Документация: В коде отсутствует документация (комментарии или docstrings) к функциям и переменным.
+
 
 import os
 import datetime
 import hashlib
 
-def reverse(input):
-    L = len(input)
+def reverse(input_str):
+    """
+    Reverses a given input string.
+    
+    Args:
+        input_str (str): The input string to be reversed.
+        
+    Returns:
+        str: The reversed string.
+    """
+    L = len(input_str)
     if (L % 2) != 0:
         return None
     else:
-        Res = ''
+        result = ''
         L = L // 2
         for i in range(L):
-            T = input[i*2] + input[i*2+1]
-            Res = T + Res
+            T = input_str[i*2] + input_str[i*2+1]
+            result = T + result
             T = ''
-        return (Res);
+        return result
 
-def merkle_root(lst): # https://gist.github.com/anonymous/7eb080a67398f648c1709e41890f8c44
+def merkle_root(lst):
+    """
+    Calculates the Merkle Root of a list of hashes.
+    
+    Args:
+        lst (list): A list of hash values.
+        
+    Returns:
+        str: The Merkle Root hash value.
+    """
     sha256d = lambda x: hashlib.sha256(hashlib.sha256(x).digest()).digest()
     hash_pair = lambda x, y: sha256d(x[::-1] + y[::-1])[::-1]
-    if len(lst) == 1: return lst[0]
+    if len(lst) == 1:
+        return lst[0]
     if len(lst) % 2 == 1:
         lst.append(lst[-1])
-    return merkle_root([hash_pair(x,y) for x, y in zip(*[iter(lst)]*2)])
+    return merkle_root([hash_pair(x, y) for x, y in zip(*[iter(lst)]*2)])
 
-def read_bytes(file,n,byte_order = 'L'):
+def read_bytes(file, n, byte_order='L'):
+    """
+    Reads and converts bytes from a file.
+    
+    Args:
+        file: The file object to read from.
+        n (int): The number of bytes to read.
+        byte_order (str): Byte order ('L' for little-endian, 'B' for big-endian).
+        
+    Returns:
+        str: The hexadecimal representation of the read bytes.
+    """
     data = file.read(n)
     if byte_order == 'L':
         data = data[::-1]
@@ -37,16 +83,27 @@ def read_bytes(file,n,byte_order = 'L'):
     return data
 
 def read_varint(file):
+    """
+    Read a variable-length integer from the given file and return it as an uppercase hexadecimal string.
+    
+    Args:
+        file (io.BufferedIOBase): An open binary file.
+        
+    Returns:
+        str: The variable length integer as an uppercase hexadecimal string.
+    """
     b = file.read(1)
     bInt = int(b.hex(),16)
     c = 0
     data = ''
+    
     if bInt < 253:
         c = 1
         data = b.hex().upper()
     if bInt == 253: c = 3
     if bInt == 254: c = 5
     if bInt == 255: c = 9
+
     for j in range(1,c):
         b = file.read(1)
         b = b.hex().upper()
@@ -54,9 +111,7 @@ def read_varint(file):
     return data
 
 dirA = './blocks/' # Directory where blk*.dat files are stored
-#dirA = sys.argv[1]
 dirB = './result/' # Directory where to save parsing results
-#dirA = sys.argv[2]
 
 fList = os.listdir(dirA)
 fList = [x for x in fList if (x.endswith('.dat') and x.startswith('blk'))]
@@ -70,6 +125,8 @@ for i in fList:
     t = dirA + nameSrc
     resList.append('Start ' + t + ' in ' + str(datetime.datetime.now()))
     print ('Start ' + t + ' in ' + str(datetime.datetime.now()))
+
+
     f = open(t,'rb')
     tmpHex = ''
     fSize = os.path.getsize(t)
