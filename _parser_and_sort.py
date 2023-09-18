6b63f43b481012e917acf9e8d49aa2132e733094
dmgorshenin <gorsh03@gmail.com>
2023-09-18 22:55:12 +0400

    Исправлены отступы, импорты, максимальные длины строк, строки документации

--- _parser_and_sort.py
+++ _parser_and_sort.py
@@ -6,6 +6,7 @@ import time
 import cv2
 import numpy as np
 import requests
+
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
@@ -13,62 +14,53 @@ dog_path = "C:/Users/User/nuck figgers/dataset/dog"
 cat_path = "C:/Users/User/nuck figgers/dataset/cat"
 
 
-def get_images(count_imgs, path, typename, indexs=None):
-
+def get_images(count_images, path, type_name, indexs=None):
     if not os.path.exists("C:/Users/User/nuck figgers/dataset"):
         os.mkdir("C:/Users/User/nuck figgers/dataset")
     if not os.path.exists(path):
         os.mkdir(path)
-
     count = 0
-
     for i in tqdm(range(1, 99), desc="Страница "):
-
         letters = string.ascii_lowercase
         rand_string = ''.join(random.sample(letters, 7))
         _headers = {
             "User-Agent": rand_string
         }
-        if not typename == "cat":
-            url = f"https://yandex.ru/images/search?p={i}&text=dog&uinfo=sw-1920-sh-1080-ww-912-wh-881-pd-1.100000023841858-wp-16x9_2560x1440&lr=51&rpt=image"
+        if not type_name == "cat":
+            url = f"https://yandex.ru/images/search?p={i}&text=dog& \
+                    uinfo=sw-1920-sh-1080-ww-912-wh-881-pd-1.100000 \
+                    023841858-wp-16x9_2560x1440&lr=51&rpt=image"
         else:
-            url = f"https://yandex.ru/images/search?p={i}&text=cat&uinfo=sw-1920-sh-1080-ww-878-wh-924-pd-1-wp-16x9_1920x1080&lr=51&rpt=image"
-
+            url = f"https://yandex.ru/images/search?p={i}&text=cat& \
+                    uinfo=sw-1920-sh-1080-ww-878-wh-924-pd-1-wp-16x \
+                    9_1920x1080&lr=51&rpt=image"
         req = requests.get(url, headers=_headers)
-
         soup = BeautifulSoup(req.text, "html.parser")
-
         src_list = []
-
         for link in soup.find_all("img", class_="justifier__thumb"):
             src_list.append(link.get("src"))
-
         for img_url in tqdm(src_list, desc="Скчивание картинок "):
             if img_url.find("n=13") != -1:
                 try:
                     source = "https:" + img_url
                     picture = requests.get(source)
-
                     if indexs != None:
                         name_file = str(indexs[count])
                     else:
                         name_file = str(count)
-
-                    with open(os.path.join(path, f"{name_file.zfill(4)}.jpg"), "wb") as out:
+                    with open(os.path.join(path,
+                              f"{name_file.zfill(4)}.jpg"),
+                              "wb") as out:
                         out.write(picture.content)
                         out.close()
-
                     time.sleep(0.5)
-
                     count += 1
-                    if(count == count_imgs):
+                    if(count == count_images):
                         return i
-
                 except Exception:
                     if indexs[count] == IndexError:
                         return None
                     print("Error in: \n", count)
-
 
 
 def is_similar(image1, image2):
@@ -76,60 +68,45 @@ def is_similar(image1, image2):
 
 
 def check_images(path):
-
     images = []
     images2 = []
     for file_name in os.listdir(path):
         images.append((cv2.imread(os.path.join(path, file_name)),
                       os.path.join(path, file_name)))
-
         images2.append((cv2.imread(os.path.join(path, file_name)),
                         os.path.join(path, file_name)))
-
     indexs = []
-
     for im, fname in tqdm(images):
-        
         for im2, fname2 in images2:
             if(fname == fname2):
                 continue
-
             if is_similar(im, im2):
                 print(fname, fname2)
-                
                 temp = fname2.replace(f"{path}\\", "")
                 temp = temp.replace(".jpg", "")
                 indexs.append(int(temp))
-
                 try:
                     os.remove(fname2)
                 except Exception as e:
                     print(e)
                     pass
-
     return indexs
 
 
 def main():
     count_find = 1250
-
-    # get_images(count_find, dog_path, "dog")
-
-    # print("Пауза")
-    # for sec in tqdm(range(1, 121), ):
-    #     time.sleep(1)
-
+    get_images(count_find, dog_path, "dog")
+    print("Пауза")
+    for sec in tqdm(range(1, 121), ):
+        time.sleep(1)
     get_images(count_find, cat_path, "cat")
-
-    # indexs = check_images(dog_path)
-    # get_images(count_find, dog_path, "dog", indexs)
-
-    # print("Пауза")
-    # for sec in tqdm(range(1, 121), ):
-    #     time.sleep(1)
-
-    # indexs = check_images(cat_path)
-    # get_images(count_find, cat_path, "cat", indexs)
+    indexs = check_images(dog_path)
+    get_images(count_find, dog_path, "dog", indexs)
+    print("Пауза")
+    for sec in tqdm(range(1, 121), ):
+        time.sleep(1)
+    indexs = check_images(cat_path)
+    get_images(count_find, cat_path, "cat", indexs)
 
 
 if __name__ == "__main__":
--- _writer.py
+++ _writer.py
@@ -13,95 +13,91 @@ def write_annotation(path_to_dataset: str, csv_path: str) -> str or None:
 
     Returns:
         str or None: Returns path to csvfile or None 
-    """
-    if not csv_path.find(".csv"):
-        field_names = ["The Absolute Way", "Relative Way", "Class"]
-        with open(csv_path, "w", newline="", encoding="utf8") as f:
-            printer = csv.DictWriter(
-                f, fieldnames=field_names, delimiter=";")
-            printer.writeheader()
-
-        class_name = "cat"
-        path_to_cat = os.path.join(path_to_dataset, class_name)
-        num_files = len([f for f in os.listdir(path_to_cat)
-                        if os.path.isfile(os.path.join(path_to_cat, f))])
-
-        for i in tqdm(range(0, num_files)):
-            with open(csv_path, "a", newline="", encoding="utf8") as f:
-                print_in_file = csv.DictWriter(
-                    f, fieldnames=field_names, delimiter=";")
-
-                path = os.path.join(path_to_cat, f"{str(i).zfill(4)}.jpg")
-
-                if os.path.isfile(path):
-                    print_in_file.writerow({"The Absolute Way": path,
-                                            "Relative Way": os.path.relpath(path),
-                                            "Class": class_name})
-
-        class_name = "dog"
-        path_to_dog = os.path.join(path_to_dataset, class_name)
-        num_files = len([f for f in os.listdir(path_to_dog)
-                        if os.path.isfile(os.path.join(path_to_dog, f))])
-
-        for i in tqdm(range(0, num_files)):
-            with open(csv_path, "a", newline="", encoding="utf8") as f:
-                print_in_file = csv.DictWriter(
-                    f, fieldnames=field_names, delimiter=";")
-
-                path = os.path.join(path_to_dog, f"{str(i).zfill(4)}.jpg")
-
-                if os.path.isfile(path):
-                    print_in_file.writerow({"The Absolute Way": path,
-                                            "Relative Way": os.path.relpath(path),
-                                            "Class": class_name})
-
-    else:
-        field_names = ["The Absolute Way", "Relative Way", "Class"]
-        with open(csv_path, "w", newline="", encoding="utf8") as f:
-            printer = csv.DictWriter(
-                f, fieldnames=field_names, delimiter=";")
-            printer.writeheader()
-
-        class_name = "cat"
-        path_to_cat = os.path.join(path_to_dataset, class_name)
-        num_files = len([f for f in os.listdir(path_to_cat)
-                        if os.path.isfile(os.path.join(path_to_cat, f))])
-
-        for i in tqdm(range(0, num_files)):
-            with open(csv_path, "a", newline="", encoding="utf8") as f:
-                print_in_file = csv.DictWriter(
-                    f, fieldnames=field_names, delimiter=";")
-
-                path = os.path.join(path_to_cat, f"{str(i).zfill(4)}.jpg")
-
-                if os.path.isfile(path):
-                    print_in_file.writerow({"The Absolute Way": path,
-                                            "Relative Way": os.path.relpath(path),
-                                            "Class": class_name})
-
-        class_name = "dog"
-        path_to_dog = os.path.join(path_to_dataset, class_name)
-        num_files = len([f for f in os.listdir(path_to_dog)
-                        if os.path.isfile(os.path.join(path_to_dog, f))])
-
-        for i in tqdm(range(0, num_files)):
-            with open(csv_path, "a", newline="", encoding="utf8") as f:
-                print_in_file = csv.DictWriter(
-                    f, fieldnames=field_names, delimiter=";")
-
-                path = os.path.join(path_to_dog, f"{str(i).zfill(4)}.jpg")
-
-                if os.path.isfile(path):
-                    print_in_file.writerow({"The Absolute Way": path,
-                                            "Relative Way": os.path.relpath(path),
-                                            "Class": class_name})
         
-        return csv_path
-                    
+    """
+    
+    if not csv_path.find(".csv"):
+        field_names = ["The Absolute Way",
+                       "Relative Way",
+                       "Class"
+                      ]
+        with open(csv_path, "w", newline="", encoding="utf8") as f:
+            printer = csv.DictWriter(
+                f, fieldnames=field_names, delimiter=";")
+            printer.writeheader()
+        class_name = "cat"
+        path_to_cat = os.path.join(path_to_dataset, class_name)
+        num_files = len([f for f in os.listdir(path_to_cat)
+                        if os.path.isfile(os.path.join(path_to_cat, f))])
+        for i in tqdm(range(0, num_files)):
+            with open(csv_path, "a", newline="", encoding="utf8") as f:
+                print_in_file = csv.DictWriter(
+                    f, fieldnames=field_names, delimiter=";")
+                path = os.path.join(path_to_cat,
+                                    f"{str(i).zfill(4)}.jpg"
+                                   )
+                if os.path.isfile(path):
+                    print_in_file.writerow({"The Absolute Way": path,
+                                            "Relative Way": os.path.relpath(path),
+                                            "Class": class_name})
+        class_name = "dog"
+        path_to_dog = os.path.join(path_to_dataset, class_name)
+        num_files = len([f for f in os.listdir(path_to_dog)
+                        if os.path.isfile(os.path.join(path_to_dog, f))])
+        for i in tqdm(range(0, num_files)):
+            with open(csv_path, "a", newline="", encoding="utf8") as f:
+                print_in_file = csv.DictWriter(
+                    f, fieldnames=field_names, delimiter=";")
+                path = os.path.join(path_to_dog, f"{str(i).zfill(4)}.jpg")
+                if os.path.isfile(path):
+                    print_in_file.writerow({"The Absolute Way": path,
+                                            "Relative Way": os.path.relpath(path),
+                                            "Class": class_name})
+    else:
+        field_names = ["The Absolute Way",
+                       "Relative Way",
+                       "Class"
+                      ]
+        with open(csv_path, "w", newline="", encoding="utf8") as f:
+            printer = csv.DictWriter(
+                f, fieldnames=field_names, delimiter=";")
+            printer.writeheader()
+        class_name = "cat"
+        path_to_cat = os.path.join(path_to_dataset, class_name)
+        num_files = len([f for f in os.listdir(path_to_cat)
+                        if os.path.isfile(os.path.join(path_to_cat, f))])
+        for i in tqdm(range(0, num_files)):
+            with open(csv_path, "a", newline="", encoding="utf8") as f:
+                print_in_file = csv.DictWriter(
+                    f, fieldnames=field_names, delimiter=";")
+                path = os.path.join(path_to_cat,
+                                    f"{str(i).zfill(4)}.jpg"
+                                   )
+                if os.path.isfile(path):
+                    print_in_file.writerow({"The Absolute Way": path,
+                                            "Relative Way": os.path.relpath(path),
+                                            "Class": class_name})
+        class_name = "dog"
+        path_to_dog = os.path.join(path_to_dataset, class_name)
+        num_files = len([f for f in os.listdir(path_to_dog)
+                        if os.path.isfile(os.path.join(path_to_dog, f))])
+        for i in tqdm(range(0, num_files)):
+            with open(csv_path, "a", newline="", encoding="utf8") as f:
+                print_in_file = csv.DictWriter(
+                    f, fieldnames=field_names, delimiter=";")
+                path = os.path.join(path_to_dog,
+                                    f"{str(i).zfill(4)}.jpg"
+                                   )
+                if os.path.isfile(path):
+                    print_in_file.writerow({"The Absolute Way": path,
+                                            "Relative Way": os.path.relpath(path),
+                                            "Class": class_name})
+        return csv_path                    
 
 
 def main() -> None:
     """Main function"""
+
     path_to_dataset = "C:/Users/User/nuck figgers/dataset"
     csv_path = "dataset.csv"
     write_annotation(path_to_dataset, csv_path)
