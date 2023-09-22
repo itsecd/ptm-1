3ff129ba366cf6d9ad17c180296f22157ebe38a1
Миша Чукарев <mishakrut11@gmail.com>
2023-09-22 14:33:44 +0400

    removed unnecessary comm and empty lines

--- lab.py
+++ lab.py
@@ -21,19 +21,16 @@ from torch.utils.data import DataLoader, Dataset
 
 
 def generate_key_pair(private_key_path: str,  public_key_path: str, symmetric_key_path: str) -> None:
-    """Эта функция генерирует пару ключей(ассиметричный и симмитричный) гибридной системы, а после сохроняет их в файлы.
+    """Эта функция генерирует пару ключей(ассиметричный и симметричный) гибридной системы, а после сохроняет их в файлы.
 
     Args:
         private_key_path (str): путь до секретного ключа
         public_key_path (str): путь до общедоступного ключа
         symmetric_key_path (str): путь до симмитричного ключа
     """
-    # генерация ассимитричного ключа
     private_key = rsa.generate_private_key(
         public_exponent=65537, key_size=2048)
     public_key = private_key.public_key()
-
-    # сохранение ассимитричного ключа
     with open(public_key_path, 'wb') as f_p, open(private_key_path, 'wb') as f_c:
         f_p.write(public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
@@ -43,14 +40,9 @@ def generate_key_pair(private_key_path: str,  public_key_path: str, symmetric_ke
                                             format=serialization.PrivateFormat.TraditionalOpenSSL,
                                             encryption_algorithm=serialization.NoEncryption()))
 
-     # Генерация ключа симметричного алгоритма
     symmetric_key = os.urandom(16)
-
-    # Шифрование ключа симметричного алгоритма
     ciphertext = public_key.encrypt(symmetric_key, padding.OAEP(mgf=padding.MGF1(
         algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-
-    # Сохранение зашифрованного ключа симметричного алгоритма
     with open(symmetric_key_path, "wb") as f:
         f.write(ciphertext)
 
@@ -64,25 +56,18 @@ def encrypt_data(initial_file_path: str, private_key_path: str, encrypted_symmet
         encrypted_symmetric_key_path (str): путь до зашифрованного симмитричного ключа
         encrypted_file_path (str): путь куда шифруются данных
     """
-    # Чтение закрытого ключа
     with open(private_key_path, "rb") as f:
         private_key = serialization.load_pem_private_key(
             f.read(), password=None)
-
-    # Расшифровка симметричного ключа
     with open(encrypted_symmetric_key_path, "rb") as f:
         encrypted_symmetric_key = f.read()
-
     symmetric_key = private_key.decrypt(encrypted_symmetric_key, padding.OAEP(
         mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-
-    # Шифрование текстового файла
     iv = os.urandom(16)
     cipher = Cipher(algorithms.SM4(symmetric_key), modes.CBC(
         iv))
     encryptor = cipher.encryptor()
     padder = sym_padding.PKCS7(128).padder()
-
     with open(initial_file_path, "rb") as f_in, open(encrypted_file_path, "wb") as f_out:
         f_out.write(iv)
         while chunk := f_in.read(128):
@@ -94,7 +79,7 @@ def encrypt_data(initial_file_path: str, private_key_path: str, encrypted_symmet
 
 
 def decrypt_data(encrypted_file_path: str, private_key_path: str, encrypted_symmetric_key_path: str, decrypted_file_path: str) -> None:
-    """эта функция дешифрует данные используя симмитричный и ассиметричные ключи, а так же сохраняет результат по указыному пути
+    """эта функция дешифрует данные используя симметричный и ассиметричные ключи, а так же сохраняет результат по указыному пути
 
     Args:
         encrypted_file_path (str): путь до зашифрованных данных
@@ -105,13 +90,10 @@ def decrypt_data(encrypted_file_path: str, private_key_path: str, encrypted_symm
     with open(private_key_path, "rb") as f:
         private_key = serialization.load_pem_private_key(
             f.read(), password=None, backend=default_backend())
-
     with open(encrypted_symmetric_key_path, "rb") as f:
         encrypted_symmetric_key = f.read()
-
     symmetric_key = private_key.decrypt(encrypted_symmetric_key, padding.OAEP(
         mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-    # Дешифрование текстового файла
     with open(encrypted_file_path, "rb") as f_in, open(decrypted_file_path, "wb") as f_out:
         iv = f_in.read(16)  # Считывание IV из файла
         cipher = Cipher(algorithms.SM4(symmetric_key),
@@ -153,15 +135,12 @@ class CustomImageDataset(Dataset):
 class CNN(nn.Module):
     def __init__(self) -> None:
         super(CNN, self).__init__()
-
         self.conv_1 = nn.Conv2d(3, 16, kernel_size=3, padding=0, stride=2)
         self.conv_2 = nn.Conv2d(16, 32, kernel_size=3, padding=0, stride=2)
         self.conv_3 = nn.Conv2d(32, 64, kernel_size=3, padding=0, stride=2)
-
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.1)
         self.max_pool = nn.MaxPool2d(2)
-
         self.fc_1 = nn.Linear(576, 10)
         self.fc_2 = nn.Linear(10, 1)
 
@@ -172,7 +151,6 @@ class CNN(nn.Module):
         output = self.max_pool(output)
         output = self.relu(self.conv_3(output))
         output = self.max_pool(output)
-
         output = torch.nn.Flatten()(output)
         output = self.relu(self.fc_1(output))
         output = torch.nn.Sigmoid()(self.fc_2(output))
@@ -208,14 +186,11 @@ def main():
         epoch_accuracy = 0
         epoch_val_accuracy = 0
         epoch_val_loss = 0
-
         for data, label in train_dataloader:
             data = data.to(device)
             label = label.to(device)
-
             output = model(data)
             loss = criterion(output, label.unsqueeze(dim=1).to(torch.float))
-
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -223,17 +198,14 @@ def main():
                 label[j]) else 0 for j in range(label.shape[0])])).mean()
             epoch_accuracy += acc / len(train_dataloader)
             epoch_loss += loss / len(train_dataloader)
-
         accuracy_values.append(epoch_accuracy)
         loss_values.append(epoch_loss)
         print('Epoch : {}, train accuracy : {}, train loss : {}'.format(
             epoch + 1, epoch_accuracy, epoch_loss))
-
         model.eval()
         for data, label in val_dataloader:
             data = data.to(device)
             label = label.to(device)
-
             output = model(data)
             loss_val = criterion(
                 output, label.unsqueeze(dim=1).to(torch.float))
@@ -241,54 +213,43 @@ def main():
                 label[j]) else 0 for j in range(label.shape[0])])).mean()
             epoch_val_accuracy += acc_val / len(val_dataloader)
             epoch_val_loss += loss_val / len(val_dataloader)
-
         accuracy_val_values.append(epoch_val_accuracy)
         loss_val_values.append(epoch_val_loss)
-
         print('Epoch : {}, val accuracy : {}, val loss : {}'.format(
             epoch + 1, epoch_val_accuracy, epoch_val_loss))
-
     plt.figure(1, figsize=(15, 5))
     plt.title('Train accuracy')
     plt.plot(range(len(accuracy_values)), accuracy_values, color="green")
     plt.legend(["Acchukarevacy"])
-
     plt.figure(2, figsize=(15, 5))
     plt.title('Train loss')
     plt.plot(range(len(accuracy_values)), [float(value.detach())
              for value in loss_values], color="blue")
     plt.legend(["Loss"])
-
     plt.figure(3, figsize=(15, 5))
     plt.title('valid accuracy')
     plt.plot(range(len(accuracy_val_values)),
              accuracy_val_values, color="green")
     plt.legend(["Accuracy"])
-
     plt.figure(4, figsize=(15, 5))
     plt.title('valid loss')
     plt.plot(range(len(accuracy_val_values)), [float(value.detach())
              for value in loss_val_values], color="blue")
     plt.legend(["Loss"])
     plt.show()
-
     model.eval()
     test_loss = 0
     test_accuracy = 0
-
     for data, label in test_dataloader:
         data = data.to(device)
         label = label.to(device)
-
         output = model(data)
-
         acc = np.array(([1 if (1 if output[j][0].detach() >= 0.5 else 0) == int(
             label[j]) else 0 for j in range(4)])).mean()
         test_accuracy += acc / len(test_dataloader)
         test_loss += float(loss.detach()) / len(test_dataloader)
     print('test_accuracy=', test_accuracy, ' ', 'test_loss=', test_loss)
     print('end')
-
     settings = {
         'initial_file': 'file\initial_file.txt',
         'encrypted_file': 'file\encrypted_file.txt',
@@ -307,7 +268,6 @@ def main():
             break
         else:
             break
-
     while True:
         answ = input(
             'Что вы хотите сделать?\nВведите первую букву слова\n(Г)генерация ключей\n(Ш)ифрование текста\n(Д)ешифрование текста\nдля выхода 1\n')
@@ -326,7 +286,6 @@ def main():
         else:
             print('Программа завершена')
             break
-
     with open('settings.json', 'w') as fp:
         json.dump(settings, fp)
 
