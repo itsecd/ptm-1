3372575bab45330564d5f586b0fbcfc74dd27752
artyEZ <abitur.arty@gmail.com>
2023-10-05 10:35:27 +0400

    Add one more file and add code

---
+++ refactoring_2.py
@@ -0,0 +1,109 @@
+import numpy
+import pandas as pd
+import datetime
+import os
+from typing import Union
+import autopep8
+
+
+def formatted_file(input_file: str) -> pd.DataFrame:
+
+    df = pd.read_csv(input_file)
+    df["Day"] = pd.to_datetime(df.Day, format="%Y-%m-%d")
+    df["Day"] = df["Day"].dt.date
+    return df
+
+
+def get_data(input_file: str, date: datetime.date) -> Union[numpy.float64, None]:
+
+    if os.path.exists(input_file):
+        df = formatted_file(input_file)
+        for i in range(0, df.shape[0], 1):
+            if str(df["Day"].iloc[i]).replace("-", "") == str(date).replace("-", ""):
+                return df.iloc[i]["Exchange rate"]
+        return None
+    raise FileNotFoundError
+
+
+def get_data_xy(input_file_x: str, input_file_y: str,
+                date: datetime.date) -> Union[numpy.float64, None]:
+
+    if os.path.exists(input_file_x) and os.path.exists(input_file_y):
+
+        df_x = pd.read_csv(input_file_x)
+        df_y = pd.read_csv(input_file_y)
+        index = -1
+
+        for i in range(0, df_x.shape[0], 1):
+            if df_x["Day"].iloc[i].replace("-", "") == str(date).replace("-", ""):
+                index = i
+                break
+
+        if index >= 0:
+            return df_y.iloc[index]["Exchange rate"]
+        return None
+
+    raise FileNotFoundError
+
+
+def get_data_from_week_and_years(input_directory: str, date: datetime.date) -> Union[numpy.float64, None]:
+
+    int_datetime = int(date.strftime('%Y%m%d'))
+
+    if os.path.exists(input_directory):
+        for root, dirs, files in os.walk(input_directory):
+            for filename in files[0: -1:]:
+                words = filename.split('_')
+                words[1] = words[1].split('.', 1)[0]
+
+                for word in range(int(words[0]), int(words[1]) - 1, -1):
+                    if word == int_datetime:
+                        df = pd.read_csv(os.path.join(root, filename))
+
+                        for i in range(0, df.shape[0], 1):
+                            if df["Day"].iloc[i].replace("-", "") == str(date).replace("-", ""):
+                                return df.iloc[i]["Exchange rate"]
+                    pass
+    raise FileNotFoundError
+
+
+def tuple_for_next_data() -> tuple:
+    
+    input_file = "C:/Users/artyo/Desktop/dataset.csv"
+    if os.path.exists(input_file):
+        df = pd.read_csv(input_file)
+        for data in df["Day"]:
+            i = df.index[df["Day"] == data]
+            yield data, *df.loc[i]["Exchange rate"].values
+    raise FileNotFoundError
+
+
+if __name__ == "__main__":
+    try:
+        file = "C:/Users/artyo/Desktop/dataset.csv"
+        file_x = "C:/Users/artyo/PycharmProjects/labs/lab2/1/X.csv"
+        file_y = "C:/Users/artyo/PycharmProjects/labs/lab2/1/Y.csv"
+        directory_for_weeks = "C:/Users/artyo/PycharmProjects/labs/lab2/3/"
+        directory_for_years = "C:/Users/artyo/PycharmProjects/labs/lab2/2/"
+
+        existing_date = datetime.date(2022, 9, 15)
+        nonexistent_date = datetime.date(1991, 5, 12)
+
+        # print(get_data(file, existing_date))
+        # print(get_data(file, nonexistent_date))
+        #
+        # print(get_data_xy(file_x, file_y, existing_date))
+        # print(get_data_xy(file_x, file_y, nonexistent_date))
+        #
+        # print(get_data_from_week_and_years(directory_for_weeks, existing_date))
+        # print(get_data_from_week_and_years(directory_for_weeks, nonexistent_date))
+        #
+        # print(get_data_from_week_and_years(directory_for_years, existing_date))
+        # print(get_data_from_week_and_years(directory_for_years, nonexistent_date))
+
+        it = tuple_for_next_data()
+        while True:
+            print(next(it))
+
+    except FileNotFoundError:
+        print("No such file exists!")
---
+++ refactoring_3.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import autopep8
+
+
+def formatted_file(input_file: str) -> pd.DataFrame:
+    
+    df = pd.read_csv(input_file)
+
+    df["Day"] = pd.to_datetime(df.Day, format="%Y-%m-%d")
+    df["Day1"] = df["Day"].dt.date  # превращаю Str to datatime64[ns]
+    df["Year"] = df["Day"].dt.year
+    df["Week"] = df["Day"].dt.isocalendar().week
+    return df
+
+
+def clear_file(df: pd.DataFrame) -> pd.DataFrame:
+    
+    del df["Year"]
+    del df["Week"]
+    del df["Day1"]
+    return df
+
+
+def range_of_years(input_file: str) -> list:
+    
+    df = formatted_file(input_file)
+
+    start_range = df["Year"].iat[0]
+    end_range = df["Year"].iat[-1]
+    return [start_range, end_range]
+
+
+def max_week(df: pd.DataFrame) -> int:
+    
+    start_range = df[df["Week"] == df["Week"].max()]
+    value = start_range["Week"].values[0]
+    return value
+
+
+def min_week(df: pd.DataFrame) -> int:
+    
+    end_range = df[df["Week"] == df["Week"].min()]
+    value = end_range["Week"].values[0]
+    return value
+
+
+def write_to_file(input_file: str) -> None:
+    
+    df = formatted_file(input_file)
+    range_of_years_list = range_of_years(input_file)
+
+    for years in range(range_of_years_list[0], range_of_years_list[1] - 1, -1):
+        lf = df[df["Year"] == years]
+
+        for weeks in range(
+                max_week(lf), min_week(lf) - 1, -1):
+            try:
+                sf = lf[lf["Week"] == weeks]
+                if sf.empty:
+                    break
+                elif sf.shape[0] == 1:
+                    data = str(sf["Day1"].iloc[0]).replace("-", "") + "_" + str(sf["Day1"].iloc[0]).replace("-", "")
+                else:
+                    data = str(sf["Day1"].iloc[0]).replace("-", "") + "_" + str(sf["Day1"].iloc[-1]).replace("-", "")
+                clear_file(sf)
+
+                sf.to_csv(data + ".csv", index=False)
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    file = "C:/Users/artyo/Desktop/dataset.csv"
+    write_to_file(file)
