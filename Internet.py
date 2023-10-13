604b14976b2995f450bc182d699e930155fc7324
the80hz <dnl.vlgv@mail.ru>
2023-10-14 00:16:20 +0400

    changed the type of function, corrected the logic

--- Internet.py
+++ Internet.py
@@ -4,20 +4,27 @@ from rich import print
 from rich.console import Console
 
 
-def main():
-    timeout = 1
-    internet = False
+def check_internet_connection(timeout=1, max_retries=5):
     console = Console()
+    retries = 0
 
-    while not internet:
+    while retries < max_retries:
         print('Проверка соединения')
         try:
             requests.head("https://www.google.com/", timeout=timeout)
             print('Соединение с интернетом есть')
-            internet = True
             time.sleep(2)
             console.clear()
+            return True
         except requests.ConnectionError:
+            retries += 1
             print("Интернета нет, повторная попытка через несколько секунд.")
             time.sleep(5)
             console.clear()
+
+    print("Не удалось установить соединение после всех попыток.")
+    return False
+
+
+if __name__ == "__main__":
+    check_internet_connection()
