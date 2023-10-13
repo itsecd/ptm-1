3758eab0518fab12d835f82959a5983ebf364a03
the80hz <dnl.vlgv@mail.ru>
2023-10-14 00:30:34 +0400

    more exception manipulation

--- tele.py
+++ tele.py
@@ -46,149 +46,153 @@ def main():
     while not turn_on:
         try:
             Internet.check_internet_connection()
-
-            print('Made by rus152')
-            print('')
-
-            bot = telebot.TeleBot(token)
-
-            print('Бот в данный момент ОнЛайн')
-
-            @bot.message_handler(commands=['start'])
-            def send_welcome(message):
-                bot.send_message(message.chat.id, 'Я НАГИБАТОР3000, только напиши и комп отключится')
-                markup = get_keyboard()
-                bot.reply_to(message, "Вывод кнопок", parse_mode='html', reply_markup=markup)
-
-            @bot.message_handler(commands=['hibernation'])
-            def send(message):
-                execute_and_reply(bot, message, 'shutdown /h', "Комп переходит в режим гибернация")
-
-            @bot.message_handler(commands=['lock🔒'])
-            def send(message):
-                execute_and_reply(bot, message, 'rundll32.exe user32.dll, LockWorkStation', "Блокировка компа 🔒")
-
-            @bot.message_handler(commands=['shutdown'])
-            def send(message):
-                sec = types.ReplyKeyboardMarkup(resize_keyboard=True)
-                but0 = types.KeyboardButton("0")
-                but1 = types.KeyboardButton("1800")
-                but2 = types.KeyboardButton("3600")
-                but3 = types.KeyboardButton("7200")
-                but4 = types.KeyboardButton("21600")
-                but5 = types.KeyboardButton("Назад")
-                sec.add(but0, but1, but2, but3, but4, but5)
-                msg: str = bot.send_message(message.chat.id, 'Через сколько отключить компьютер?(В секундах)',
-                                            parse_mode='html', reply_markup=sec)
-                bot.register_next_step_handler(msg, test)
-
-            def test(message):
-                txt = message.text
-                print(txt)
-                markup = get_keyboard()
-
-                if txt == "Назад":
-                    bot.reply_to(message, "Возврат", parse_mode='html', reply_markup=markup)
-                    return
-
-                try:
-                    if int(txt) >= 18000:
-                        cas = int(txt) / 3600
-                        print(cas)
-                        bot.reply_to(message, "Компьютер выключится через " + str(round(int(cas))) + " часов 🕑",
-                                     parse_mode='html',
-                                     reply_markup=markup)
-
-                    if (int(txt) >= 7200) and (int(txt) < 18000):
-                        cas = int(txt) / 3600
-                        print(cas)
-                        bot.reply_to(message, "Компьютер выключится через " + str(round(int(cas))) + " часа 🕑",
-                                     parse_mode='html',
-                                     reply_markup=markup)
-
-                    if (int(txt) > 3599) and (int(txt) < 7200):
-                        cas = int(txt) / 3600
-                        print(cas)
-                        bot.reply_to(message, "Компьютер выключится через " + str(round(int(cas))) + " час 🕑",
-                                     parse_mode='html',
-                                     reply_markup=markup)
-
-                    if (int(txt) > 60) and (int(txt) < 120):
-                        menu = int(txt) / 60
-                        print(menu)
-                        bot.reply_to(message, "Компьютер выключится через " + str(round(int(menu))) + " минуту 🕑",
-                                     parse_mode='html',
-                                     reply_markup=markup)
-
-                    if (int(txt) > 119) and (int(txt) < 300):
-                        menu = int(txt) / 60
-                        print(menu)
-                        bot.reply_to(message, "Компьютер выключится через " + str(round(int(menu))) + " минуты 🕑",
-                                     parse_mode='html',
-                                     reply_markup=markup)
-
-                    if (int(txt) > 299) and (int(txt) < 3600):
-                        menu = int(txt) / 60
-                        print(menu)
-                        bot.reply_to(message, "Компьютер выключится через " + str(round(int(menu))) + " минут 🕑",
-                                     parse_mode='html',
-                                     reply_markup=markup)
-
-                    if (int(txt) > 0) and (int(txt) < 61):
-                        menu = int(txt) / 60
-                        print(menu)
-                        bot.reply_to(message, "Компьютер выключится через " + txt + " секунд 🕑", parse_mode='html',
-                                     reply_markup=markup)
-
-                    if int(txt) == 0:
-                        bot.reply_to(message, "Компьютер отключается ", parse_mode='html',
-                                     reply_markup=markup)
-                    os.system('shutdown /s /t ' + txt)
-
-                except Exception as _ex:
-                    bot.reply_to(message,
-                                 str(_ex),
-                                 parse_mode='html',
-                                 reply_markup=markup)
-                    return
-
-            @bot.message_handler(commands=['cancel'])
-            def send(message):
-                execute_and_reply(bot, message, 'shutdown /a', "Отключение отменено")
-
-            @bot.message_handler(commands=['Online'])
-            def send(message):
-                bot.reply_to(message, "В данный момент комп онлайн", )
-                toaster = win10toast.ToastNotifier()
-                toaster.show_toast("БОТЯРА 🔔", "Все знают, что комп онлайн", icon_path="icon.ico")
-
-            @bot.message_handler(commands=['Screen'])
-            def send(message):
-                bot.reply_to(message, "Запрос отправлен", )
-                mss().shot(mon=1)
-                try:
-                    mss().shot(mon=2)
-                except Exception as _ex:
-                    print(_ex)
-                toaster = win10toast.ToastNotifier()
-                toaster.show_toast("БОТЯРА 🔔", "Запрос на скриншот", icon_path="icon.ico")
-                bot.send_message(message.chat.id, 'Скриншот сделан')
-                bot.send_document(message.chat.id, document=open('monitor-1.png', 'rb'))
-                try:
-                    bot.send_document(message.chat.id, document=open('monitor-2.png', 'rb'))
-                except Exception as _ex:
-                    print(_ex)
-                os.remove('monitor-1.png')
-                try:
-                    os.remove('monitor-2.png')
-                except Exception as _ex:
-                    print(_ex)
-
-            bot.polling(none_stop=True)
         except Exception as ex:
-            print("Произошла ошибка, попытка перезапуска", ex)
+            print(f"Ошибка при проверке интернета: {ex}")
             time.sleep(5)
             console.clear()
+            continue
+
+        print('Made by rus152')
+        print('')
+
+        bot = telebot.TeleBot(token)
+        print('Бот в данный момент ОнЛайн')
+
+        @bot.message_handler(commands=['start'])
+        def send_welcome(message):
+            bot.send_message(message.chat.id, 'Я НАГИБАТОР3000, только напиши и комп отключится')
+            markup = get_keyboard()
+            bot.reply_to(message, "Вывод кнопок", parse_mode='html', reply_markup=markup)
+
+        @bot.message_handler(commands=['hibernation'])
+        def send(message):
+            execute_and_reply(bot, message, 'shutdown /h', "Комп переходит в режим гибернация")
+
+        @bot.message_handler(commands=['lock🔒'])
+        def send(message):
+            execute_and_reply(bot, message, 'rundll32.exe user32.dll, LockWorkStation', "Блокировка компа 🔒")
+
+        @bot.message_handler(commands=['shutdown'])
+        def send(message):
+            sec = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            but0 = types.KeyboardButton("0")
+            but1 = types.KeyboardButton("1800")
+            but2 = types.KeyboardButton("3600")
+            but3 = types.KeyboardButton("7200")
+            but4 = types.KeyboardButton("21600")
+            but5 = types.KeyboardButton("Назад")
+            sec.add(but0, but1, but2, but3, but4, but5)
+            msg: str = bot.send_message(message.chat.id, 'Через сколько отключить компьютер?(В секундах)',
+                                        parse_mode='html', reply_markup=sec)
+            bot.register_next_step_handler(msg, test)
+
+        def test(message):
+            txt = message.text
+            print(txt)
+            markup = get_keyboard()
+
+            if txt == "Назад":
+                bot.reply_to(message, "Возврат", parse_mode='html', reply_markup=markup)
+                return
+
+            try:
+                if int(txt) >= 18000:
+                    cas = int(txt) / 3600
+                    print(cas)
+                    bot.reply_to(message, "Компьютер выключится через " + str(round(int(cas))) + " часов 🕑",
+                                 parse_mode='html',
+                                 reply_markup=markup)
+
+                if (int(txt) >= 7200) and (int(txt) < 18000):
+                    cas = int(txt) / 3600
+                    print(cas)
+                    bot.reply_to(message, "Компьютер выключится через " + str(round(int(cas))) + " часа 🕑",
+                                 parse_mode='html',
+                                 reply_markup=markup)
+
+                if (int(txt) > 3599) and (int(txt) < 7200):
+                    cas = int(txt) / 3600
+                    print(cas)
+                    bot.reply_to(message, "Компьютер выключится через " + str(round(int(cas))) + " час 🕑",
+                                 parse_mode='html',
+                                 reply_markup=markup)
+
+                if (int(txt) > 60) and (int(txt) < 120):
+                    menu = int(txt) / 60
+                    print(menu)
+                    bot.reply_to(message, "Компьютер выключится через " + str(round(int(menu))) + " минуту 🕑",
+                                 parse_mode='html',
+                                 reply_markup=markup)
+
+                if (int(txt) > 119) and (int(txt) < 300):
+                    menu = int(txt) / 60
+                    print(menu)
+                    bot.reply_to(message, "Компьютер выключится через " + str(round(int(menu))) + " минуты 🕑",
+                                 parse_mode='html',
+                                 reply_markup=markup)
+
+                if (int(txt) > 299) and (int(txt) < 3600):
+                    menu = int(txt) / 60
+                    print(menu)
+                    bot.reply_to(message, "Компьютер выключится через " + str(round(int(menu))) + " минут 🕑",
+                                 parse_mode='html',
+                                 reply_markup=markup)
+
+                if (int(txt) > 0) and (int(txt) < 61):
+                    menu = int(txt) / 60
+                    print(menu)
+                    bot.reply_to(message, "Компьютер выключится через " + txt + " секунд 🕑", parse_mode='html',
+                                 reply_markup=markup)
+
+                if int(txt) == 0:
+                    bot.reply_to(message, "Компьютер отключается ", parse_mode='html',
+                                 reply_markup=markup)
+                os.system('shutdown /s /t ' + txt)
+
+            except Exception as _ex:
+                bot.reply_to(message,
+                             str(_ex),
+                             parse_mode='html',
+                             reply_markup=markup)
+                return
+
+        @bot.message_handler(commands=['cancel'])
+        def send(message):
+            execute_and_reply(bot, message, 'shutdown /a', "Отключение отменено")
+
+        @bot.message_handler(commands=['Online'])
+        def send(message):
+            bot.reply_to(message, "В данный момент комп онлайн", )
+            toaster = win10toast.ToastNotifier()
+            toaster.show_toast("БОТЯРА 🔔", "Все знают, что комп онлайн", icon_path="icon.ico")
+
+        @bot.message_handler(commands=['Screen'])
+        def send(message):
+            bot.reply_to(message, "Запрос отправлен", )
+            mss().shot(mon=1)
+            try:
+                mss().shot(mon=2)
+            except Exception as _ex:
+                print(_ex)
+            toaster = win10toast.ToastNotifier()
+            toaster.show_toast("БОТЯРА 🔔", "Запрос на скриншот", icon_path="icon.ico")
+            bot.send_message(message.chat.id, 'Скриншот сделан')
+            bot.send_document(message.chat.id, document=open('monitor-1.png', 'rb'))
+            try:
+                bot.send_document(message.chat.id, document=open('monitor-2.png', 'rb'))
+            except Exception as _ex:
+                print(_ex)
+            os.remove('monitor-1.png')
+            try:
+                os.remove('monitor-2.png')
+            except Exception as _ex:
+                print(_ex)
+
+    try:
+        bot.polling(none_stop=True)
+    except Exception as ex:
+        print(f"Ошибка при работе бота: {ex}")
+        time.sleep(5)
 
 
 if __name__ == '__main__':
