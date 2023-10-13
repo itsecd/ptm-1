2f81701152a1a9afbc7a77e68089bf5e0b1afc29
the80hz <dnl.vlgv@mail.ru>
2023-10-14 00:17:18 +0400

    fstrings, and exception checking

--- Internet.py
+++ Internet.py
@@ -4,21 +4,21 @@ from rich import print
 from rich.console import Console
 
 
-def check_internet_connection(timeout=1, max_retries=5):
+def check_internet_connection(timeout=1, max_retries=10):
     console = Console()
     retries = 0
 
     while retries < max_retries:
-        print('Проверка соединения')
+        print('Проверка соединения...')
         try:
             requests.head("https://www.google.com/", timeout=timeout)
             print('Соединение с интернетом есть')
             time.sleep(2)
             console.clear()
             return True
-        except requests.ConnectionError:
+        except (requests.ConnectionError, requests.Timeout, requests.RequestException):
             retries += 1
-            print("Интернета нет, повторная попытка через несколько секунд.")
+            print(f"Интернета нет, попытка {retries}/{max_retries}. Повторная попытка через 5 секунд.")
             time.sleep(5)
             console.clear()
 
