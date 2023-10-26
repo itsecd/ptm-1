062d82a3ca41087f5794cf9df020758ac1105799
lenayak <113600536+lenayak@users.noreply.github.com>
2023-10-27 02:29:45 +0400

    edited long message

--- client.py
+++ client.py
@@ -36,21 +36,8 @@ class TelemetryClient(object):
 
     def receive(self) -> str:
         if (self._diagnosticMessageJustSent):
-            message = """\
-LAST TX rate................ 100 MBPS\r\n
-HIGHEST TX rate............. 100 MBPS\r\n
-LAST RX rate................ 100 MBPS\r\n
-HIGHEST RX rate............. 100 MBPS\r\n
-BIT RATE.................... 100000000\r\n
-WORD LEN.................... 16\r\n
-WORD/FRAME.................. 511\r\n
-BITS/FRAME.................. 8192\r\n
-MODULATION TYPE............. PCM/FM\r\n
-TX Digital Los.............. 0.75\r\n
-RX Digital Los.............. 0.10\r\n
-BEP Test.................... -5\r\n
-Local Rtrn Count............ 00\r\n
-Remote Rtrn Count........... 00"""
+            with open("message.txt") as file:
+                message = "\n".join(file.readlines())
             self._diagnosticMessageJustSent = False
         else:
             message = ""
