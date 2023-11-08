4b6d79beee1f29b356745ff6d0663e1ee3a71c23
danyala1 <d.mayasov@mail.ru>
2023-11-08 18:11:41 +0400

    TennisGameDefactored3
    Изменены названия переменных в классе TennisGameDefactored3

--- tennis.py
+++ tennis.py
@@ -115,22 +115,22 @@ class TennisGameDefactored3:
     def __init__(self, player1Name, player2Name):
         self.player1Name = player1Name
         self.player2Name = player2Name
-        self.player1 = 0
-        self.player2 = 0
+        self.player1points = 0
+        self.player2points = 0
 
-    def won_point(self, number):
-        if number == self.player1Name:
-            self.player1 += 1
+    def won_point(self, name):
+        if name == self.player1Name:
+            self.player1points += 1
         else:
-            self.player2 += 1
+            self.player2points += 1
 
     def score(self):
-        if (self.player1 < 4 and self.player2 < 4):
+        if (self.player1points < 4 and self.player2points < 4):
             players = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = players[self.player1]
-            return s + "-All" if (self.player1 == self.player2) else s + "-" + players[self.player2]
+            result = players[self.player1points]
+            return result + "-All" if (self.player1points == self.player2points) else result + "-" + players[self.player2points]
         else:
-            if (self.player1 == self.player2):
+            if (self.player1points == self.player2points):
                 return "Deuce"
-            s = self.player1Name if self.player1 > self.player2 else self.player2Name
-            return "Advantage " + s if ((self.player1-self.player2) * (self.player1-self.player2) == 1) else "Win for " + s
+            result = self.player1Name if self.player1points > self.player2points else self.player2Name
+            return "Advantage " + result if ((self.player1points-self.player2points) * (self.player1points-self.player2points) == 1) else "Win for " + result
